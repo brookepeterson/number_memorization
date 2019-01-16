d3dda064c010a9ee9bffa9252fcc7c1e3bd2af03
Brooke Peterson <brookepeterson08@gmail.com>
2019-01-16 09:37:35 -0500

    Create FinalMainScript.m

---
+++ FinalMainScript.m
@@ -0,0 +1,7 @@
+%Brooke Peterson 
+%May 10, 2018 
+%This script runs my final code.
+
+clear all %clears workspace of all variables 
+close all %closes all figures 
+run('guilab.m') %runs main code 
